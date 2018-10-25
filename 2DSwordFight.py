732898161e5ca39e5451ccaf90d401f827eba4f3
bigmaxattack37 <bigmaxmarcus@gmail.com>
2018-10-24 21:44:46 -0500

    Added enviroment problem with certain sides for the collider

--- 2DSwordFight.py
+++ 2DSwordFight.py
@@ -21,6 +21,61 @@ startLoop = False
 
 startTime = time.time()
 objects = []
+class Enviroment:
+    def __init__(self,sizex,sizey):
+        self.grid = []
+        self.sizex = sizex
+        self.sizey = sizey
+        self.rectSizex = (root.winfo_screenwidth()/self.sizex)
+        self.rectSizey = (root.winfo_screenheight()/self.sizey)
+
+        i = 0
+        while i < sizex:
+            z = 0
+            self.ygrid = []
+            while z < sizey:
+                self.ygrid.append(0)
+                z += 1
+            i += 1
+            self.grid.append(self.ygrid)
+
+    def setBlock(self,x,y,on=1):
+        self.grid[y-1][x-1] = on
+        if startLoop:
+            self.render()
+    def setRow(self,x,on=1):
+        i = 0
+        while i < self.sizex:
+            self.grid[x-1][i] = on
+            i += 1
+        if startLoop:
+            self.render()
+    def setColumn(self,y,on=1):
+        i = 0
+        while i < self.sizey:
+            self.grid[i][y-1] = on
+            i += 1
+        if startLoop:
+            self.render()
+    def createGrid(self):
+        i = 0
+        while i < self.sizey:
+            canvas.create_line(0,self.rectSizey*i,root.winfo_screenwidth(),self.rectSizey*i,fill = 'blue')
+            i += 1
+        z = 0
+        while z < self.sizex:
+            canvas.create_line(self.rectSizex*z,0,self.rectSizex*z,root.winfo_screenheight(),fill = 'blue')
+            z += 1
+    def render(self):
+        canvas.delete(ALL)
+        i = 0
+        while i < len(self.grid):
+            z = 0
+            while z < len(self.grid[i]):
+                if self.grid[i][z] == 1:
+                    canvas.create_rectangle((z+1)*self.rectSizex,(i+1)*self.rectSizey,z*self.rectSizex,i*self.rectSizey,fill='red')
+                z += 1
+            i += 1
 class Character:
     def __init__(self,name,left,right,jump,moveAim,gunInput,file):
         self.size = root.winfo_screenwidth()/25
@@ -38,8 +93,8 @@ class Character:
         self.mouseRight = False
         self.aimcolor = "blue"
         self.aimcolorchange = 0
-        self.speedx = root.winfo_screenwidth()/250
-        self.speedy = root.winfo_screenheight()/250
+        self.speedx = root.winfo_screenwidth()/2500
+        self.speedy = root.winfo_screenheight()/2500
         self.x = root.winfo_screenwidth()/2
         self.y = (root.winfo_screenheight()/100)*92 - self.size/2
         self.originaly = (root.winfo_screenheight()/100)*92 - self.size/2
@@ -62,8 +117,10 @@ class Character:
         self.swordLengthY = -self.size/3#-root.winfo_screenheight()/30
         self.swordDirction = 1
         self.cursorSpeed = root.winfo_screenwidth()/8000
-        self.jumpSpeed = root.winfo_screenheight()/100
-        self.gravSpeed = root.winfo_screenheight()/100
+        self.jumpSpeed = root.winfo_screenheight()/1500
+        self.gravSpeed = root.winfo_screenheight()/1500
+        self.hit = 0
+        self.stillHit = 0
         if self.gunInput == 'mouse_button':
             root.bind('<Button-1>',self.fire,add='+')
         else:
@@ -79,17 +136,10 @@ class Character:
             self.right = True
             self.swordDirction = 1
         if event.keysym in self.jumpInput:
-            if self.y == self.originaly:
+            spoty = int((self.y + self.size/2)/enviroment.rectSizey)
+            spotx = int((self.x + self.size/2)/enviroment.rectSizex)
+            if enviroment.grid[spoty][spotx] == 1:
                 self.jump = True
-        if not self.mouseInput == 'motion':
-            if event.keysym == self.mouseInput[0]:
-                self.mouseUp = True
-            if event.keysym == self.mouseInput[1]:
-                self.mouseRight = True
-            if event.keysym == self.mouseInput[2]:
-                self.mouseDown = True
-            if event.keysym == self.mouseInput[3]:
-                self.mouseLeft = True
 
     def stopMove(self,event):
 
@@ -97,90 +147,61 @@ class Character:
             self.left = False
         if event.keysym in self.rightInput:
             self.right = False
-        if not self.mouseInput == 'motion':
-            if event.keysym == self.mouseInput[0]:
-                self.mouseUp = False
-            if event.keysym == self.mouseInput[1]:
-                self.mouseRight = False
-            if event.keysym == self.mouseInput[2]:
-                self.mouseDown = False
-            if event.keysym == self.mouseInput[3]:
-                self.mouseLeft = False
 
     def move(self):
-        if self.left:
-            if self.x > 0 + self.size/2:
-                self.x -= self.speedx
-        if self.right:
-            if self.x < root.winfo_screenwidth() - self.size/2:
-                self.x += self.speedx
+        spoty = (self.y + self.size/2)/enviroment.rectSizey
+        spot_x_leftside = (self.x - self.size/2)/enviroment.rectSizex
+        spot_x_rightside = (self.x + self.size/2)/enviroment.rectSizex
+
+        if self.hit == 0:
+            if self.left:
+                if self.x > 0 + self.size/2:
+                    self.x -= self.speedx
+            if self.right:
+                if self.x < root.winfo_screenwidth() - self.size/2:
+                    self.x += self.speedx
+        else:
+            if self.stillHit <= 200:
+                if self.x < root.winfo_screenwidth() - self.size/2 and self.x > 0 + self.size/2:
+                    self.x += self.speedx * 2 * self.hit
+                self.stillHit += 1
+            else:
+                self.hit = 0
         if self.jump:
-            if self.y == self.originaly or self.jump:
-                if self.jumped < self.size * 3:
-                    self.y -= self.jumpSpeed
-                    self.jumped += self.jumpSpeed
+            if (not enviroment.grid[int(spoty)][int(spot_x_leftside)] == 1 or not enviroment.grid[int(spoty)][int(spot_x_rightside)] == 1) or self.jump:
+                if self.jumped < enviroment.rectSizey*2.75 and self.y - self.size/2 > 0:
+                        self.y -= self.jumpSpeed
+                        self.jumped += self.jumpSpeed
                 else:
                     self.jump = False
                     self.jumped = 0
-        if not self.y >= self.originaly and not self.jump:
+
+        if (not enviroment.grid[int(spoty)][int(spot_x_leftside)] == 1 or not enviroment.grid[int(spoty)][int(spot_x_rightside)] == 1) and not self.jump:
             self.y += self.gravSpeed
-    def moveCursor(self):
-        if self.mouseInput == 'motion':
-            self.mousePosX = root.winfo_pointerx()
-            self.mousePosY = root.winfo_pointery()
-            if self.mousePosX < self.x + self.gunStrength and self.mousePosX > self.x - self.gunStrength:
-                self.cursorX = self.mousePosX
-            else:
-                if self.mousePosX > self.x:
-                    self.cursorX = self.x + self.gunStrength
-                if self.mousePosX < self.x:
-                    self.cursorX = self.x - self.gunStrength
-            if self.mousePosY < self.y + self.gunStrength and self.mousePosY > self.y - self.gunStrength:
-                self.cursorY = self.mousePosY
-            else:
-                if self.mousePosY > self.y:
-                    self.cursorY = self.y + self.gunStrength
-                if self.mousePosY < self.y:
-                    self.cursorY = self.y - self.gunStrength
-        else:
-            if self.mouseUp and self.cursorY - 1 < self.y + self.gunStrength and self.cursorY - 1 > self.y - self.gunStrength:
-                self.cursorY -= self.cursorSpeed
-            if self.mouseRight and self.cursorX + 1< self.x + self.gunStrength and self.cursorX + 1 > self.x - self.gunStrength:
-                self.cursorX += self.cursorSpeed
-            if self.mouseDown and self.cursorY + 1< self.y + self.gunStrength and self.cursorY + 1 > self.y - self.gunStrength:
-                self.cursorY += self.cursorSpeed
-            if self.mouseLeft and self.cursorX - 1< self.x + self.gunStrength and self.cursorX - 1 > self.x - self.gunStrength:
-                self.cursorX -= self.cursorSpeed
-            if not self.cursorY < self.y + self.gunStrength:
-                self.cursorY = self.y + self.gunStrength - 1
-            if not self.cursorY > self.y - self.gunStrength:
-                self.cursorY = self.y - self.gunStrength + 1
-            if not self.cursorX < self.x + self.gunStrength:
-                self.cursorX = self.x + self.gunStrength - 1
-            if not self.cursorX > self.x - self.gunStrength:
-                self.cursorX = self.x - self.gunStrength + 1
+
     def fire(self,event):
         if event.keysym in self.gunInput or self.gunInput == 'mouse_button':
             i = 0
             self.aimcolor = "black"
             self.aimcolorchange = time.time()
             while i < len(objects):
-                objects[i].isHit(range(self.x,self.x + self.swordLengthX*self.swordDirction),self.y + self.size/2,self.name)
+                if self.swordDirction == 1:
+                    objects[i].isHit(range(int(self.x),int(self.x + self.swordLengthX*self.swordDirction)),self.y + self.size/2,self.swordDirction,self.name)
+                else:
+                    objects[i].isHit(range(int(self.x + self.swordLengthX*self.swordDirction),int(self.x)),self.y + self.size/2,self.swordDirction,self.name)
                 i += 1
-    def isHit(self,x,bottomY,name):
+    def isHit(self,x,bottomY,direction,name):
         if not self.name == name:
-            for z in range(self.x - self.size/2,self.x + self.size/2):
+            for z in range(int(self.x - self.size/2),int(self.x + self.size/2)):
                 if z in x:
-                    print('worling')
                     if self.y <= bottomY:
-                        self.dead = True
-                        objects.remove(objects[self.name])
-        '''
-        if x > self.x - self.size/2 and x < self.x + self.size/2:
-            if y > self.y - self.size/2 and y < self.y + self.size/2:
-                self.dead = True
-                objects.remove(objects[self.name])
-'''
+                        if direction == -1:
+                            self.stillHit = 0
+                            self.hit = -1
+                        else:
+                            self.stillHit = 0
+                            self.hit = 1
+
     def render(self):
         canvas.delete(self.graphics)
         canvas.delete(self.aim)
@@ -188,7 +209,7 @@ class Character:
             self.aimcolor = "gray"
             self.aimcolorchange = 0
         if not self.dead:
-            self.aim = canvas.create_line(self.x,self.y,self.x + self.swordLengthX*self.swordDirction,self.y + self.swordLengthY,fill = self.aimcolor,width = 1)
+            self.aim = canvas.create_line(self.x,self.y,self.x + self.swordLengthX*self.swordDirction,self.y + self.swordLengthY,fill = self.aimcolor,width = 6)
             if not 'none_' in self.file:
                 self.graphics = canvas.create_image(self.x,self.y,image = self.avatarBlue)
             else:
@@ -202,6 +223,16 @@ def start(event):
 
 root.bind("<Key>",start)
 root.update()
+
+enviroment = Enviroment(10,10)
+enviroment.setRow(10)
+enviroment.setBlock(1,10,0)
+enviroment.setBlock(10,10,0)
+enviroment.setBlock(2,8)
+enviroment.setBlock(4,6)
+enviroment.setBlock(6,4)
+enviroment.setBlock(8,2)
+enviroment.setBlock(10,1)
 while startLoop == False:
     root.update()
     if startLoop == True:
@@ -210,12 +241,15 @@ while startLoop == False:
         objects.append(character)
         player = Character(1,['Left'],['Right'],['Up','Down'],'motion','mouse_button','none_red')
         objects.append(player)
+        enviroment.render()
+
+        enviroment.createGrid()
         while True:
-            character.moveCursor()
+
             character.move()
             character.render()
 
-            player.moveCursor()
             player.move()
             player.render()
+
             root.update()
